58ba088049495dbf2e040b51aadaf2bdf9871c32
Costas Kirgoussios <ckir1508@hotmail.com>
2025-06-01 06:59:46 +0300

    Initial commit

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,5 @@
 
-import path from 'path';
-import { fileURLToPath } from 'node:url';
-import { dirname, basename, sep, extname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const script_name = basename(__filename, extname(__filename));
+import path from 'node:path';
 
 async function loadModule(moduleFile) {
     const module = await import(moduleFile);
@@ -15,17 +10,9 @@ export default {
     markets: {
         nasdaq: {
             apiNasdaq: async () => {
-                const module = await loadModule(path.join(__dirname, 'lib', 'common', 'markets', 'nasdaq', 'apiNasdaq.mjs'));
+                const module = await loadModule(pathjoin('.', 'lib', 'common', 'markets', 'nasdaq', 'apiNasdaq.mjs'));
                 return module;
             },
         }
     },
-    // apiNasdaqFetch: async (url) => {
-    //     const module = await loadModule('apiNasdaq');
-    //     return module(url);
-    // },
-    // apiErrorToString: async (status) => {
-    //     const module = await loadModule('apiNasdaq');
-    //     return module.apiErrorToString(status);
-    // }
 }
