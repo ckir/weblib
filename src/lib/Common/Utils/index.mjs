1e66efb409a91450f331f6fddeb87f233e3d9364
Costas Kirgoussios <ckir1508@hotmail.com>
2026-01-17 11:46:58 +0200

    Fix Linux

---
+++ src/lib/Common/Utils/index.mjs
@@ -0,0 +1,20 @@
+import ArrayUtils from './Misc/ArrayUtils.mjs';
+import ConfigUtils from './Misc/ConfigUtils.mjs';
+import CryptoUtils from './Misc/CryptoUtils.mjs';
+import DateTimeUtils from './Misc/DateTimeUtils.mjs';
+import StringUtils from './Misc/StringUtils.mjs';
+import SystemInfoCollector from './Misc/SystemInfo.mjs';
+import SystemUtils from './Misc/SystemUtils.mjs';
+import * as Alerts from './Alerts/index.mjs';
+
+const Misc = {
+    ArrayUtils: ArrayUtils,
+    ConfigUtils: ConfigUtils,
+    CryptoUtils: CryptoUtils,
+    DateTimeUtils: DateTimeUtils,
+    StringUtils: StringUtils,
+    SystemInfo: SystemInfoCollector,
+    SystemUtils: SystemUtils,
+}
+
+export { Alerts, Misc }
