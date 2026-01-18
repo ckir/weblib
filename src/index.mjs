3bfa0bd6157b4f879fe3602b3e48e7e0a3015806
Costas Kirgoussios <ckir1508@hotmail.com>
2026-01-18 14:41:35 +0200

    Export Vendored

--- src/index.mjs
+++ src/index.mjs
@@ -1,25 +1,32 @@
 
 import ky from 'ky';
-export { ky };
 import { serializeError, deserializeError } from 'serialize-error';
-export { serializeError, deserializeError };
 import safeStringify from 'safe-stringify';
 export { safeStringify };
 import * as luxon from 'luxon';
-export { luxon };
 
-import * as Common from './lib/Common/index.mjs';
-import * as Cloud from './lib/Cloud/index.mjs';
+// import * as Common from './lib/Common/index.mjs';
+// import * as Cloud from './lib/Cloud/index.mjs';
 
 globalThis.WebLib = {};
 if (!globalThis.logger) {
-    const { default: Logger } = await import('./lib/Common/Logger/LoggerDummy.mjs');
+    const { default: Logger } = await import('./lib/Common/Loggers/LoggerDummy.mjs');
     globalThis.logger = new Logger();
 }
+
+const Common = await import('./lib/Common/index.mjs');
+const Cloud = await import('./lib/Cloud/index.mjs');
 
 export const WebLib = {
     Common,
     Cloud,
+    Vendored: {
+        ky,
+        serializeError,
+        deserializeError,
+        safeStringify,
+        luxon
+    }
 };
 
 export default WebLib;
